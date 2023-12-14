b15db2423b4cfd9616684aed0bc1be0dd13089c9
vinhweb <vjhnguyen@gmail.com>
2023-12-14 07:53:19 +0700

    Update getSitemapPages.js

--- utils/getSitemapPages.js
+++ utils/getSitemapPages.js
@@ -4,15 +4,7 @@ export default function getSitemapPages(item) {
   const items = [];
   for (let i = 1; i <= Math.ceil(item.total / sitemapPerPage); i++) {
     let url = `${frontendUrl}/sitemap/${item.name}_sitemap${i}.xml`;
-    items.push(
-      ` 
-        <sitemap>
-           <loc>
-              ${url}
-          </loc>
-      </sitemap>
-      `
-    );
+    items.push(`<sitemap><loc>${url}</loc></sitemap>`);
   }
   return items.join("");
 }
